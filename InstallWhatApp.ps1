ddbc3673fd62f5fefe84338bfb93bcca13db4726
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2020-12-15 09:45:54 -0800

    Update InstallWhatApp.ps1

--- InstallWhatApp.ps1
+++ InstallWhatApp.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest 'Invoke-WebRequest 'http://tlu.dl.delivery.mp.microsoft.com/filestreamingservice/files/e1d3c771-f9a9-499f-bfe1-f200bb0c2f37?P1=1608004943&P2=402&P3=2&P4=RDLAl6k4QFClKavOFRdGDho1uzwlKHopttVjtevxy%2bakkZSnWNfrmxYxzC7fumbwU8UiV1J1j3X7jluQgbZ3GA%3d%3d' -OutFile 'C:\whatsAppAppx'
+Invoke-WebRequest 'http://tlu.dl.delivery.mp.microsoft.com/filestreamingservice/files/e1d3c771-f9a9-499f-bfe1-f200bb0c2f37?P1=1608004943&P2=402&P3=2&P4=RDLAl6k4QFClKavOFRdGDho1uzwlKHopttVjtevxy%2bakkZSnWNfrmxYxzC7fumbwU8UiV1J1j3X7jluQgbZ3GA%3d%3d' -OutFile 'C:\whatsAppAppx'
 Get-AppxPackage -allusers | foreach {Add-AppxPackage -Path "C:\whatsAppAppx"}
