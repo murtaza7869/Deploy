02b8cfc16a85cb8aa0b3b3649fe17d59d312638a
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2020-12-14 17:42:34 -0800

    Add files via upload

---
+++ InstallSnapCoreFirst.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest 'http://tlu.dl.delivery.mp.microsoft.com/filestreamingservice/files/5f98e942-7b3a-42ed-b02c-df234effb397?P1=1608004139&P2=402&P3=2&P4=gyfaXarJg3ME8Yr7%2b69ePTEg%2bq0LSfOVUQcPhHN%2fpbEuXScWgMTtFjMud3ZDHDRkRdoTeGp5l4ZIgDVuGmxu2w%3d%3d' -OutFile 'C:\SnapCoreAppx'
+Add-AppxPackage -Path "C:\SnapCoreAppx"
