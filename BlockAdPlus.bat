cdef37c643c08790f632437c61e5353f78b68a9e
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2021-01-18 18:42:46 -0800

    Update BlockAdPlus.bat

--- BlockAdPlus.bat
+++ BlockAdPlus.bat
@@ -1,1 +1,2 @@
 reg add HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist /v 1 /t REG_SZ /d cbhilkcodigmigfbnphipnnmamjfkipp
+reg add HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist /v 2 /t REG_SZ /d llpfnmnallbompdmklfkcibfpcfpncdd
