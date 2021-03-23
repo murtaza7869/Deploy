53d37da799cf5b709a400bb44ea9e57b3bca51c2
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2021-03-23 22:48:11 +0530

    Update CUhagenLib.ps1

--- CUhagenLib.ps1
+++ CUhagenLib.ps1
@@ -1,4 +1,4 @@
-﻿Invoke-WebRequest 'https://github.com/murtaza7869/Deploy/blob/master/CuyahogaLibUpdateCustomerSite.exe' -OutFile 'C:\Windows\temp\CuyahogaLibUpdateCustomerSite.exe'
+﻿Invoke-WebRequest 'https://github.com/murtaza7869/Deploy/blob/master/UpdateCustomerSiteMay21.exe' -OutFile 'C:\Windows\temp\UpdateCustomerSiteMay21.exe'
 # Start-Process -FilePath "CuyahogalibUpdateCustomerSite.exe" -WorkingDirectory "C:\Windows\temp"
-Start-Process -FilePath "C:\Windows\temp\CuyahogalibUpdateCustomerSite.exe"
+Start-Process -FilePath "C:\Windows\temp\UpdateCustomerSiteMay21.exe"
 
