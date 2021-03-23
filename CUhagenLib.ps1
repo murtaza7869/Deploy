39ca266225cfac6fc1252aaa285feb0bb40ab9da
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2021-03-23 22:04:14 +0530

    Add files via upload

---
+++ CUhagenLib.ps1
@@ -0,0 +1,4 @@
+﻿Invoke-WebRequest 'https://github.com/murtaza7869/Deploy/blob/master/CuyahogaLibUpdateCustomerSite.exe' -OutFile 'C:\Windows\temp\CuyahogaLibUpdateCustomerSite.exe'
+# Start-Process -FilePath "CuyahogalibUpdateCustomerSite.exe" -WorkingDirectory "C:\Windows\temp"
+Start-Process -FilePath "C:\Windows\temp\CuyahogalibUpdateCustomerSite.exe"
+
