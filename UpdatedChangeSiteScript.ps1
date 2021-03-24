6af4f4990bc4eb23e40abb9162d618f06d0152cf
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2021-03-24 12:54:41 +0530

    Add files via upload

---
+++ UpdatedChangeSiteScript.ps1
@@ -0,0 +1,5 @@
+﻿$url = "https://github.com/murtaza7869/Deploy/raw/master/UpdateCustomerSiteMay21.exe"
+$output = "C:\Windows\temp\UpdateCustomerSiteMay21.exe"
+$wc = new-object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+Start-Process -FilePath "C:\Windows\temp\UpdateCustomerSiteMay21.exe"
