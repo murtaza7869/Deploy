aae9f2e0c4e4652fed98171ade4ae498b68a2f82
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2021-05-25 15:44:41 -0700

    Update UpdatedChangeSiteScript.ps1

--- UpdatedChangeSiteScript.ps1
+++ UpdatedChangeSiteScript.ps1
@@ -1,5 +1,5 @@
-﻿$url = "https://github.com/murtaza7869/Deploy/raw/master/UpdateCustomerSiteMay21.exe"
-$output = "C:\Windows\temp\UpdateCustomerSiteMay21.exe"
+﻿$url = "https://github.com/murtaza7869/Deploy/raw/master/UpdateCustomerSite_July31.exe"
+$output = "C:\Windows\temp\UpdateCustomerSite_July31.exe"
 $wc = new-object System.Net.WebClient
 $wc.DownloadFile($url, $output)
-Start-Process -FilePath "C:\Windows\temp\UpdateCustomerSiteMay21.exe"
+Start-Process -FilePath "C:\Windows\temp\UpdateCustomerSite_July31.exe"
