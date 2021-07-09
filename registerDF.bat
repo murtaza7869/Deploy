516ad763c177f5d150063950bf5edf860a83da50
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2021-07-09 09:42:53 -0700

    Register DF Mof files

---
+++ registerDF.bat
@@ -0,0 +1,6 @@
+cd "C:\Program Files (x86)\Faronics\Deep Freeze\Install C-0\"
+mofcomp FaronicsWebProduct.mof
+timeout /t 5
+mofcomp FaronicsWebProduct_v1.mof
+timeout /t 5
+mofcomp DeepFreezeWorkstation.mof
