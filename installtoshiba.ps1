6df8c160531363f61383b39149322290fbf38500
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2021-07-29 16:51:22 -0700

    Update installtoshiba.ps1

--- installtoshiba.ps1
+++ installtoshiba.ps1
@@ -1,5 +1,3 @@
-
-Set-ExecutionPolicy RemoteSigned
 md C:\temp\toshiba
 Invoke-WebRequest 'https://business.toshiba.com/downloads/KB/f1Ulds/18128/eb4-ebn-Uni-3264bit-7212483517.zip?_ga=2.155413755.1507334097.1627496542-1831136956.1627042885' -Outfile 'C:\Temp\toshiba.zip'
 ping -n 5 127.0.0.1
