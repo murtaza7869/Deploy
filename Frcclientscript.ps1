b2627dfc814d745c2b9e92aca75ae41b39c13a5d
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2021-09-24 16:48:59 -0700

    Update and rename Frcclient script.ps1 to Frcclientscript.ps1

--- Frcclientscript.ps1
+++ Frcclientscript.ps1
@@ -1,2 +1,3 @@
 mkdir C:\ProgramData\Faronics\RemoteConnect
 Invoke-WebRequest 'https://github.com/murtaza7869/Deploy/raw/master/FRCClientCUHag.exe' -OutFile 'C:\ProgramData\Faronics\RemoteConnect\FRCClient.EXE'
+Start-Process -NoNewWindow -FilePath "C:\ProgramData\Faronics\RemoteConnect\FRCClient.EXE"
