ddcc42b631e540bd2f7ab67c7cb1f10237aced4f
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2021-10-08 15:47:05 -0700

    Update Frcclientscript.ps1

--- Frcclientscript.ps1
+++ Frcclientscript.ps1
@@ -1,5 +1,5 @@
 mkdir C:\ProgramData\Faronics\RemoteConnect
-Invoke-WebRequest 'https://github.com/murtaza7869/Deploy/raw/master/FRCClient.exe' -OutFile 'C:\ProgramData\Faronics\RemoteConnect\FRCClient.EXE'
+Invoke-WebRequest 'https://github.com/murtaza7869/Deploy/raw/master/FRCClient.EXE' -OutFile 'C:\ProgramData\Faronics\RemoteConnect\FRCClient.EXE'
 Invoke-WebRequest 'https://github.com/murtaza7869/Deploy/raw/master/RegFRCC.reg' -OutFile 'C:\ProgramData\Faronics\RemoteConnect\RegFRCC.reg'
 reg import 'C:\ProgramData\Faronics\RemoteConnect\RegFRCC.reg'
 Start-Process -NoNewWindow -FilePath "C:\ProgramData\Faronics\RemoteConnect\FRCClient.EXE"
