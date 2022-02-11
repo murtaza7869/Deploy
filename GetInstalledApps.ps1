4cfa09f0da35dfefb4cb7fdc936877ffa78e2b0e
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2022-02-10 17:21:30 -0800

    Update GetInstalledApps.ps1

--- GetInstalledApps.ps1
+++ GetInstalledApps.ps1
@@ -1,2 +1,2 @@
 Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize | Out-File -FilePath "C:\windows\temp\InstalledProgram.log"
-cat "C:\windows\temp\InstalledProgram.log"
+cat "C:\windows\temp\InstalledProgram.log";
