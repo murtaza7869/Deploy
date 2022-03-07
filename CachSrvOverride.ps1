78cb3991d60041c82aef83350e15b55db953ed24
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2022-03-07 14:17:19 -0800

    Update CachSrvOverride.ps1

--- CachSrvOverride.ps1
+++ CachSrvOverride.ps1
@@ -1,12 +1,18 @@
 $cacheIP=$args[0]
-Remove-Item 'C:\Windows\temp\WUCoreCacheSrvFix.zip' -Recurse
+If (Test-Path -Path C:\Windows\temp\WUCoreCacheSrvFix.zip ){
+Remove-Item 'C:\Windows\temp\WUCoreCacheSrvFix.zip' -Recurse | out-null
+}
 Invoke-WebRequest 'https://faronics.digitalpigeon.com/msg/3ZRlkJ44EeydrQa5ZeB_WQ/1ksikRfRJq28xVU9KAd9FA/file/ddb3d470-9e38-11ec-a2cc-02d7d55f1e7d/download# - WUCoreCacheSrvFix - Override Cache server with reg key - WUCoreCacheSrvFix.zip' -OutFile 'C:\Windows\temp\WUCoreCacheSrvFix.zip'
-Remove-Item 'C:\ProgramData\Faronics\CustomFix' -Recurse
+If (Test-Path -Path C:\ProgramData\Faronics\CustomFix ){
+Remove-Item 'C:\ProgramData\Faronics\CustomFix' -Recurse | out-null
+}
 md C:\ProgramData\Faronics\CustomFix
 Expand-Archive -LiteralPath C:\Windows\temp\WUCoreCacheSrvFix.zip -DestinationPath C:\ProgramData\Faronics\CustomFix
 Stop-Service -Name "FWUSvc" -Force
-ping localhost -n 10
-Remove-Item 'C:\ProgramData\Faronics\StorageSpace\SUC\Org_WuCore.dis' -Recurse
+ping localhost -n 15
+If (Test-Path -Path C:\ProgramData\Faronics\StorageSpace\SUC\Org_WuCore.dis ){
+Remove-Item 'C:\ProgramData\Faronics\StorageSpace\SUC\Org_WuCore.dis' -Recurse | out-null
+}
 Rename-Item C:\ProgramData\Faronics\StorageSpace\SUC\WUCore.dll Org_WuCore.dis
 Copy-Item "C:\ProgramData\Faronics\customFix\WUCore-64bit.dll" -Destination "C:\ProgramData\Faronics\StorageSpace\SUC\WUCore.dll"
 New-Item -Path HKLM:\SOFTWARE\Faronics
@@ -15,6 +21,4 @@ Set-ItemProperty HKLM:\SOFTWARE\Faronics\CacheServerOverride -Name ServerIP -Val
 Set-ItemProperty HKLM:\SOFTWARE\Faronics\CacheServerOverride -Name PortNo -Value "7726" -Type Dword
 ping localhost -n 5
 Start-Service -Name "FWUSvc"
-ping localhost -n 10
-return
-
+ping localhost
