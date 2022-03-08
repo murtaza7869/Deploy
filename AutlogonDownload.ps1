1a1b439bcb5e2504a95966c86841c7b2bb4dcc0d
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2022-03-08 12:46:04 -0500

    Update and rename AutlogonDownload.ps1 to DownloadPrismConfg.ps1

--- AutlogonDownload.ps1
+++
@@ -1,1 +0,0 @@
-Invoke-WebRequest 'https://github.com/murtaza7869/Deploy/raw/master/Autologon64.exe' -OutFile 'C:\Windows\temp\Autologon64.exe'
---
+++ DownloadPrismConfg.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest 'https://raw.githubusercontent.com/murtaza7869/Deploy/master/prism-config.xml' -OutFile 'C:\Windows\temp\prism-config.xml'
