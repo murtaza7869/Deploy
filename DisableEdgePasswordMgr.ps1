2c195ebb0624c7f67db30004c32c7457a88f4304
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2022-08-12 12:03:01 -0700

    Add files via upload
    Disable Password Mgr in MS Edge

---
+++ DisableEdgePasswordMgr.ps1
@@ -0,0 +1,19 @@
+﻿$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main"
+
+$Name = "FormSuggest Passwords"
+
+$value = "no"
+
+IF(!(Test-Path $registryPath))
+
+  {
+
+    New-Item -Path $registryPath -Force | Out-Null
+
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null}
+
+ ELSE {
+
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null}
+    # apply the new policy immediately
+gpupdate.exe /force
