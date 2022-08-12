3b027cc961fc83b467cd273ac021a8c5f49ac553
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2022-08-12 11:54:50 -0700

    Add files via upload
    Disables IE 11 as stand alone browser

---
+++ IE11DisableScript.ps1
@@ -0,0 +1,19 @@
+﻿$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Main"
+
+$Name = "NotifyDisableIEOptions"
+
+$value = "0"
+
+IF(!(Test-Path $registryPath))
+
+  {
+
+    New-Item -Path $registryPath -Force | Out-Null
+
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null}
+
+ ELSE {
+
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null}
+    # apply the new policy immediately
+gpupdate.exe /force
