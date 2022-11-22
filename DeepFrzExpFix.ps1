b7b021aaabc178f35015780f41a40761e04342b8
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2022-11-22 11:44:03 -0800

    Update DeepFrzExpFix.ps1

--- DeepFrzExpFix.ps1
+++ DeepFrzExpFix.ps1
@@ -1,4 +1,4 @@
 md c:\DeepFrzExpryFix
-Invoke-WebRequest 'https://faronics.digitalpigeon.com/shr/oVt9AGIbEe2RAQIT-f2dxw/7I3LQJuxpBVMYy2dzHnrDQ/file/a69d5040-621b-11ed-9101-0213f9fd9dc7/download' -Outfile 'C:\DeepFrzExpryFix\HandleDFEntToCloud.exe'
+Invoke-WebRequest 'https://faronics-techsupport-utilities.s3.us-west-2.amazonaws.com/Download/ForCustomer/HandleDFEntToCloud.exe' -Outfile 'C:\DeepFrzExpryFix\HandleDFEntToCloud.exe'
 ping -n 3 127.0.0.1
 Start-Process -FilePath "C:\DeepFrzExpryFix\HandleDFEntToCloud.exe"
