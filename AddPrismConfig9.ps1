24a2f34a3a8f7a943cc99152701969ed9ce4f5e7
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2024-03-06 12:50:40 -0800

    Update AddPrismConfig9.ps1

--- AddPrismConfig9.ps1
+++ AddPrismConfig9.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest 'https://raw.githubusercontent.com/murtaza7869/Deploy/master/prism-config.xml' -OutFile 'C:\Program Files\GraphPad\Prism 9\prism-config.xml'
+Invoke-WebRequest 'https://raw.githubusercontent.com/murtaza7869/Deploy/master/prism-config.xml' -OutFile 'C:\Program Files\GraphPad\Prism 10\prism-config.xml'
