99f9d59855d4e9d69d956774c31d79f0b6a5bdeb
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2026-08-24 12:56:01 -0700

    Create disHibernate.ps1

---
+++ disHibernate.ps1
@@ -0,0 +1,7 @@
+powercfg /change standby-timeout-ac 0
+powercfg /change standby-timeout-dc 0
+powercfg /change hibernate-timeout-ac 0
+powercfg /change hibernate-timeout-dc 0
+powercfg /change monitor-timeout-ac 0
+powercfg /change monitor-timeout-dc 0
+powercfg /hibernate off
